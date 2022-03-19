2fc000c2d6c1dcef9ffc260f63e8d1160d1547d9
Vladimir Kochetkov <js.kochetkov@gmail.com>
2022-03-19 22:28:46 +0300

    done benefits for customers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import headerModal from './modules/headerModal';
 import servisesModal from './modules/servisesModal';
 import ourServices from './modules/ourServices';
+import benefits from './modules/benefits';
 
 headerModal();
 servisesModal();
 ourServices();
+benefits();
