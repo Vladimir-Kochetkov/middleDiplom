9e09be18d1670cbb86fc265c9f6fc7a80a9365d9
Vladimir Kochetkov <js.kochetkov@gmail.com>
2022-03-25 00:16:36 +0300

    fix form

--- src/index.js
+++ src/index.js
@@ -9,27 +9,53 @@ import { swiper } from "./modules/swiper";
 import timer from './modules/timer';
 import { validation } from "./modules/validation";
 
+const body = document.querySelector('body');
 
 
 calculator();
-form({
-    formId: "form1",
-    someElement: [
-        {
-            id: 'calc-total'
+if (body.classList.contains("okna")) {
+    form({
+        formId: "form1",
+        someElement: []
+    });
+    form({
+        formId: "form2",
+        someElement: []
 
-        }
-    ]
-});
-form({
-    formId: "form2",
-    someElement: [
-        {
-            id: 'calc-total'
+    });
+}
+if (body.classList.contains("balkony")) {
+    form({
+        formId: "form3",
+        someElement: [
+            {
+                id: 'calc-total'
 
-        }
-    ]
-});
+            }
+        ]
+    });
+    form({
+        formId: "form4",
+        someElement: [
+            {
+                id: 'calc-total'
+
+            }
+        ]
+    });
+}
+if (body.classList.contains("kuhni")) {
+    form({
+        formId: "form5",
+        someElement: []
+
+    });
+    form({
+        formId: "form6",
+        someElement: []
+
+    });
+}
 headerModal();
 imgModal();
 servisesModal();
