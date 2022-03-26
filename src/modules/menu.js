571a1103a82402daef8cc7ecf235c07628dd1a51
Vladimir Kochetkov <js.kochetkov@gmail.com>
2022-03-26 20:30:39 +0300

    fix burger-menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,22 @@
 const menu = () => {
-    const menuBtn = document.querySelectorAll('.navbar-toggle')[1];
-    const navigation = document.querySelector('#navigation');
-    const menu = navigation.querySelector('#navbar-collapse');
+    const navbarToogle = document.querySelectorAll('.navbar-toggle');
+    const navigationFixed = document.querySelector('#navigation-fixed');
+    const navbarCollapseFixed = document.querySelector('#navbar-collapse-fixed');
 
-    if (window.screen.width < 992) {
-        menuBtn.addEventListener('click', () => {
-            menu.style.display = "block";
-        });
-    }
+    navbarToogle[1].addEventListener('click', () => {
+        navigationFixed.style.margin = '0';
+        navigationFixed.style.display = 'block';
+        navbarCollapseFixed.style.display = 'block';
+        navbarCollapseFixed.querySelector('.navbar-order').style.display = 'block';
+    });
+
+    navbarToogle[0].addEventListener('click', () => {
+        navigationFixed.style.margin = '-60';
+        navigationFixed.style.display = 'none';
+        navbarCollapseFixed.style.display = 'none';
+        navbarCollapseFixed.querySelector('.navbar-order').style.display = 'none';
+    });
+
 };
 
 export default menu;
